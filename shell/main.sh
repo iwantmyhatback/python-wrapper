65230f62fb52801d5ba4badac1ebc4301cf35619
iwantmyhatback <iwantmyhatbackk@gmail.com>
2024-03-02 23:23:25 -0500

    include a pip upgrade

--- shell/main.sh
+++ shell/main.sh
@@ -13,21 +13,30 @@ cd "${REPO_ROOT_DIR}" || exit
 if [ -z "${ALREADY_SOURCED:-}" ]; then
     . "${REPO_ROOT_DIR}/shell/sourceEnvironment.sh"
 else
-    echo "[INFO] [ENV] Skipping additional sourcing because ALREADY_SOURCED is defined"
+    echo "[INFO] [SH_ENV] Skipping additional sourcing because ALREADY_SOURCED is defined"
+fi
+    
+if [ "${PYTHON_LOG_LEVEL}" != "DEBUG" ]; then
+    QUIET="--quiet"
 fi
 
 FULL_PYENV_LOCATION="${REPO_ROOT_DIR}/${PYENV_LOCATION}"
 
 if [ -d "${FULL_PYENV_LOCATION}" ]; then
     echo "[INFO] [PY_ENV] ${FULL_PYENV_LOCATION} does exist."
-else
+    . "${FULL_PYENV_LOCATION}/bin/activate"
+fi
+
+if [ ! -d "${FULL_PYENV_LOCATION}" ]; then
     echo "[INFO] [PY_ENV] ${FULL_PYENV_LOCATION} does not exist"
     /usr/bin/env python3 -m venv "${FULL_PYENV_LOCATION}"
+    . "${FULL_PYENV_LOCATION}/bin/activate"
+    "${FULL_PYENV_LOCATION}/bin/python" -m pip install ${QUIET} --upgrade pip
 fi
 
 . "${FULL_PYENV_LOCATION}/bin/activate"
 
-"${FULL_PYENV_LOCATION}/bin/python" -m pip install --quiet --requirement "${REPO_ROOT_DIR}/requirements.txt"
+"${FULL_PYENV_LOCATION}/bin/python" -m pip install ${QUIET} --requirement "${REPO_ROOT_DIR}/requirements.txt"
 # "${PYENV_LOCATION}/bin/python" -m pip freeze > "${REPO_ROOT_DIR}/requirements.txt"
 
 "${FULL_PYENV_LOCATION}/bin/python" -Bu "${REPO_ROOT_DIR}/python/main.py"
