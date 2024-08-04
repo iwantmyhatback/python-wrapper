cf9ea97c901492d08e60584fa65587cab5f16fd5
iwantmyhatback <iwantmyhatbackk@gmail.com>
2024-08-04 12:58:15 -0400

    prevent empty QUIET from being interpretted as a positional arg

--- shell/run.sh
+++ shell/run.sh
@@ -11,6 +11,7 @@ REPO_ROOT_DIR="$(git rev-parse --show-toplevel)"
 cd "${REPO_ROOT_DIR}" || exit 1
 
 if [ -z "${ALREADY_SOURCED:-}" ]; then
+    # shellcheck disable=SC1091
     . "${REPO_ROOT_DIR}/shell/sourceEnvironment.sh"
 else
     printf "[INFO]\t[SH_ENV] Skipping additional sourcing because ALREADY_SOURCED is defined\n"
@@ -24,17 +25,26 @@ fi
 
 if [ -d "${FULL_PYENV_LOCATION}" ]; then
     printf "[INFO]\t[PY_ENV] %s does exist\n" "${FULL_PYENV_LOCATION}"
+    # shellcheck disable=SC1091
     . "${FULL_PYENV_LOCATION}/bin/activate"
 fi
 
 if [ ! -d "${FULL_PYENV_LOCATION}" ]; then
     printf "[INFO]\t[PY_ENV] %s does not exist\n" "${FULL_PYENV_LOCATION}"
     /usr/bin/env python3 -m venv "${FULL_PYENV_LOCATION}"
+    # shellcheck disable=SC1091
     . "${FULL_PYENV_LOCATION}/bin/activate"
-    "${FULL_PYENV_LOCATION}/bin/python" -m pip install "${QUIET}" --upgrade pip
+    # shellcheck disable=SC2086
+    "${FULL_PYENV_LOCATION}/bin/python" -m pip install ${QUIET} --upgrade pip
 fi
 
-"${FULL_PYENV_LOCATION}/bin/python" -m pip install "${QUIET}" --requirement "${REPO_ROOT_DIR}/requirements.txt"
-# "${PYENV_LOCATION}/bin/python" -m pip freeze > "${REPO_ROOT_DIR}/requirements.txt"
+# shellcheck disable=SC2086
+"${FULL_PYENV_LOCATION}/bin/python" -m pip install ${QUIET} --requirement "${REPO_ROOT_DIR}/requirements.txt"
+
+if [ "${REFREEZE_REQUIREMENTS:-}" = 'TRUE' ]; then
+    printf "[INFO]\t[PY_ENV] Re-Freezing the Requirements file\n"
+    "${PYENV_LOCATION}/bin/python" -m pip freeze > "${REPO_ROOT_DIR}/requirements.txt"
+fi
+
 
 "${FULL_PYENV_LOCATION}/bin/python" -Bu "${REPO_ROOT_DIR}/python/main.py"
