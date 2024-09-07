deb2968821b35af6fc03abe4b4b2a67935920164
iwantmyhatback <iwantmyhatbackk@gmail.com>
2024-09-07 12:22:01 -0400

    script guard

--- python/main.py
+++ python/main.py
@@ -6,24 +6,31 @@ import logging as log
 ## Executions ##
 ################
 
-# Set the loggin level for this entire script set
-if osEnviron.get("LOG_LEVEL"):
-    LOG_LOCATION = str(osEnviron.get("LOG_LOCATION", "python-output.log"))
-    LOG_LEVEL = str(osEnviron.get("LOG_LEVEL", "INFO")).upper()
-    log.root.handlers = []
-    log.basicConfig(
-        level=LOG_LEVEL,
-        format="[%(levelname)s]\t%(message)s",
-        handlers=[
-            log.FileHandler(filename=LOG_LOCATION, mode='w'),
-            log.StreamHandler()
-        ]
-    )
+def main():
+    # Set the loggin level for this entire script set
+    if osEnviron.get("LOG_LEVEL"):
+        LOG_LOCATION = str(osEnviron.get("LOG_LOCATION", "python-output.log"))
+        LOG_LEVEL = str(osEnviron.get("LOG_LEVEL", "INFO")).upper()
+        log.root.handlers = []
+        log.basicConfig(
+            level=LOG_LEVEL,
+            format="[%(levelname)s]\t%(message)s",
+            handlers=[
+                log.FileHandler(filename=LOG_LOCATION, mode='w'),
+                log.StreamHandler()
+            ]
+        )
 
-log.info('[SCRIPT] Completed Environmental Setup!')
+    log.info('[SCRIPT] Completed Environmental Setup!')
 
-# Bring in some python you want to run here...
-# ie:
-# from pythonFileSomewhere import somePythonFunction
-# somePythonFunction()
-log.info('[ DONE ] Run somePythonFunction() entry point >> HERE!')
+    # Bring in some python you want to run here...
+    # ie:
+    # from pythonFileSomewhere import somePythonFunction
+    # somePythonFunction()
+    log.info('[ DONE ] Run somePythonFunction() entry point >> HERE!')
+
+
+
+
+if __name__ == "__main__":
+    main()
